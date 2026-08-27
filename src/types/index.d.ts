2d1de7c2030b1dc3167a9730687382cee76de4d4
Nix Krystik <nix@archwing.dev>
2026-08-28 01:35:33 +0800

    (types): rename `scheduler.d.ts` to `task.d.ts`.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,4 +3,4 @@ export * from './database-types.d';
 export * from './e621-types.d';
 export * from './handler.d';
 export * from './helper-types.d';
-export * from './scheduler.d';
+export * from './task';
